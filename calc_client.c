772a6b000fd164f8d6ef5fc173bd4e0ba55247b5
Jan Ferbr <honza.ferbr@gmail.com>
2021-04-17 12:50:20 +0200

    docker version edited

--- calc_client.c
+++ calc_client.c
@@ -5,6 +5,7 @@
  */
 
 #include "calc.h"
+#include <stdio.h>
 
 
 void
@@ -44,38 +45,79 @@ com_1(char *host)
 		exit (1);
 	}
 #endif	/* DEBUG */
-
-	retval_1 = innit_1(&result_1, clnt);
-	if (retval_1 != RPC_SUCCESS) {
-		clnt_perror (clnt, "call failed");
-	}
-	retval_2 = set_value_1(set_value_1_key, set_value_1_val1, set_value_1_val2, set_value_1_val3, &result_2, clnt);
-	if (retval_2 != RPC_SUCCESS) {
-		clnt_perror (clnt, "call failed");
-	}
-	retval_3 = get_value_1(get_value_1_key, &result_3, clnt);
-	if (retval_3 != RPC_SUCCESS) {
-		clnt_perror (clnt, "call failed");
-	}
-	retval_4 = modify_value_1(modify_value_1_key, modify_value_1_val1, modify_value_1_val2, modify_value_1_val3, &result_4, clnt);
-	if (retval_4 != RPC_SUCCESS) {
-		clnt_perror (clnt, "call failed");
-	}
-	retval_5 = delete_key_1(delete_key_1_key, &result_5, clnt);
-	if (retval_5 != RPC_SUCCESS) {
-		clnt_perror (clnt, "call failed");
-	}
-	retval_6 = exist_1(exist_1_key, &result_6, clnt);
-	if (retval_6 != RPC_SUCCESS) {
-		clnt_perror (clnt, "call failed");
-	}
-	retval_7 = num_items_1(&result_7, clnt);
-	if (retval_7 != RPC_SUCCESS) {
-		clnt_perror (clnt, "call failed");
-	}
-#ifndef	DEBUG
-	clnt_destroy (clnt);
-#endif	 /* DEBUG */
+    while (1){
+        printf("1. init()\n2.set_value()\n3.get_value()\n4.modify_value()\n5.delete_key()\n6.exist()\n7.num_items()\n8.exit()\nChoose one option(ex. \"1\"):");
+        scanf("%i", &msgtype);
+        switch (mes1.type) {
+            case 1://init
+            result_1 = innit_1(clnt);
+            if (result_1 == NULL) {
+                clnt_perror(clnt, "call failed:");
+            }
+            printf("function returned:%i\n",result_1);
+            break;
+            case 2://set_value
+            scanf("%s", &set_value_1_key);
+            scanf("%s", &set_value_1_val1);
+            scanf("%i", &set_value_1_val2);
+            scanf("%f", &set_value_1_val3);
+            result_2 = set_value_1(set_value_1_key, set_value_1_val1, set_value_1_val2, set_value_1_val3, clnt);
+            if (result_2 == NULL) {
+                clnt_perror(clnt, "call failed:");
+            }
+            printf("function returned:%i\n",result_2);
+            break;
+            case 3://get_value
+            scanf("%s", &get_value_1_key);
+            result_3 = get_value_1(get_value_1_key, clnt);
+            if (result_3 == NULL) {
+                clnt_perror(clnt, "call failed:")
+            }
+            printf("function returned:%i\n",result_3.res);
+            break;
+            case 4://modify value
+            scanf("%s", &modify_value_1_key);
+            scanf("%s", &modify_value_1_val1);
+            scanf("%i", &modify_value_1_val2);
+            scanf("%f", &modify_value_1_val3);
+            result_4 = modify_value_1(modify_value_1_key, modify_value_1_val1, modify_value_1_val2, modify_value_1_val3, clnt);
+            if (result_4 == NULL) {
+                clnt_perror(clnt, "call failed:");
+            }
+            printf("function returned:%i\n",result_4);
+            break;
+            case 5://delete_key
+            scanf("%s", &delete_key_1_key);
+            result_5 = delete_key_1(delete_key_1_key, clnt);
+            if (result_5 == NULL) {
+                clnt_perror(clnt, "call failed:");
+            }
+            printf("function returned:%i\n",result_5);
+            break;
+            case 6://exist
+            scanf("%s", &exist_1_key);
+            result_6 = exist_1(exist_1_key, clnt);
+            if (result_6 == NULL) {
+                clnt_perror(clnt, "call failed:");
+            }
+            printf("function returned:%i\n",result_6);
+            break;
+            case 7://num_items
+            result_7 = num_items_1(clnt);
+            if (result_7 == NULL) {
+                clnt_perror(clnt, "call failed:");
+            }
+            printf("function returned:%i\n",result_7);
+            break;
+            default:
+            perror ("Client: Invalid Arguemnt(function)");
+            #ifndef DEBUG
+                clnt_destroy (clnt);
+            #endif
+            exit (1);
+            break;
+        }
+    }
 }
 
 
--- calc_server.c
+++ calc_server.c
@@ -5,16 +5,40 @@
  */
 
 #include "calc.h"
+#include <stdio.h>
+#include <string.h>
+
+struct Element{
+    int type;
+    char key[254];
+    char value1[254];
+    int value2;
+    float value3;
+    struct Element* pNext;
+};
+
+//GLOBALS ----------
+struct Element tmp;
+struct Element* pHead = NULL;
+
+
+//FUNCTIONS  DECLARATIONS  -------------
+int addNode(char* key, char* value1, int* value2, float* value3);
+int modifyNode(char* key, char* value1, int* value2, float* value3);
+int deleteList(void);
+int searchList(char* _key);
+int deleteElement(char* key);
+struct Element* getValue(char* key);
+int numElements(void);
 
 bool_t
 innit_1_svc(int *result, struct svc_req *rqstp)
 {
 	bool_t retval;
 
-	/*
-	 * insert server code here
-	 */
-
+	*result=deleteList();
+    
+    retval=TRUE;
 	return retval;
 }
 
@@ -23,10 +47,20 @@ set_value_1_svc(char *key, char *val1, int val2, float val3, int *result,  struc
 {
 	bool_t retval;
 
-	/*
-	 * insert server code here
-	 */
-
+    strcpy(tmp.key,key);
+    strcpy(tmp.value1,val1);
+    tmp.value2=val2;
+    tmp.value3=val3;
+    
+    if(searchList(&tmp.key)==0)
+    {
+        *result = addNode(&tmp.key,&tmp.value1,&tmp.value2,&tmp.value3);
+    }
+    else{
+        *result = -1;
+    }
+    
+    retval=TRUE;
 	return retval;
 }
 
@@ -35,10 +69,18 @@ get_value_1_svc(char *key, getval *result,  struct svc_req *rqstp)
 {
 	bool_t retval;
 
-	/*
-	 * insert server code here
-	 */
-
+    strcpy(tmp.key,key);
+    if(searchList(&tmp.key)==1){
+        *result.res=0;
+        tmp=*getValue(tmp.key);
+        strcpy(*result.val1,tmp.value1); //WEIRD!!!!!
+        *result.val2=tmp.val2;
+        *result.val3=tmp.val3;
+    }
+    else{
+        *result.res=1;
+    }
+    retval=TRUE;
 	return retval;
 }
 
@@ -47,10 +89,14 @@ modify_value_1_svc(char *key, char *val1, int val2, float val3, int *result,  st
 {
 	bool_t retval;
 
-	/*
-	 * insert server code here
-	 */
-
+    strcpy(tmp.key,key);
+    strcpy(tmp.value1,val1);
+    tmp.value2=val2;
+    tmp.value3=val3;
+    
+    *result = modifyNode(&tmp.key, &tmp.value1,&tmp.value2,&tmp.value3);
+    
+    retval=TRUE;
 	return retval;
 }
 
@@ -59,10 +105,10 @@ delete_key_1_svc(char *key, int *result,  struct svc_req *rqstp)
 {
 	bool_t retval;
 
-	/*
-	 * insert server code here
-	 */
+    strcpy(tmp.key,key);
+    *result = deleteElement(&tmp.key);
 
+    retval=TRUE;
 	return retval;
 }
 
@@ -71,10 +117,10 @@ exist_1_svc(char *key, int *result,  struct svc_req *rqstp)
 {
 	bool_t retval;
 
-	/*
-	 * insert server code here
-	 */
+    strcpy(tmp.key,key);
+    *result=searchList(&tmp.key);
 
+    retval=TRUE;
 	return retval;
 }
 
@@ -83,10 +129,9 @@ num_items_1_svc(int *result, struct svc_req *rqstp)
 {
 	bool_t retval;
 
-	/*
-	 * insert server code here
-	 */
+	*result =numElements();
 
+    retval=TRUE;
 	return retval;
 }
 
@@ -101,3 +146,96 @@ com_1_freeresult (SVCXPRT *transp, xdrproc_t xdr_result, caddr_t result)
 
 	return 1;
 }
+
+int addNode(char* key, char* value1, int* value2, float* value3)
+{
+    struct Element* new = (struct Element*)malloc(sizeof(struct Element));
+    strcpy(new->key,key);
+    strcpy(new->value1,value1);
+    new->value2 = *value2;
+    new->value3 = *value3;
+    new->pNext = pHead;
+    pHead = new;
+    return 0;
+}
+int deleteList()
+{
+    struct Element* tmp = NULL;
+    while(pHead != NULL){
+        tmp = pHead->pNext;
+        free(pHead);
+        pHead = tmp;
+    }
+    return 0;
+}
+int searchList(char* _key)
+{
+    struct Element* tmp = pHead;
+    while(tmp != NULL)
+    {
+        if(strcmp(_key, tmp->key) == 0)
+        return 1;
+        tmp = tmp->pNext;
+    }
+    return 0;//element does not exsist
+}
+struct Element* getValue(char* key)
+{
+    struct Element* tmp = pHead;
+    while(tmp != NULL)
+    {
+        if(!strcmp(key, tmp->key))
+        return tmp;
+        tmp = tmp->pNext;
+    }
+    return NULL;//element does not exsist
+}
+int modifyNode(char* key, char* value1, int* value2, float* value3)
+{
+    struct Element* tmp = pHead;
+    while(tmp != NULL)
+    {
+        if(!strcmp(key, tmp->key))
+        {
+            strcpy(tmp->value1, value1);
+            tmp->value2 = *value2;
+            tmp->value3 = *value3;
+            return 0;
+        }
+        tmp = tmp->pNext;
+    }
+    return -1;//element does not exsist
+}
+int deleteElement(char* key)
+{
+    struct Element* prev = NULL;
+    struct Element* tmp = pHead;
+    while(tmp)
+    {
+        if(!strcmp(key, tmp->key))
+        {
+            if(prev!=NULL)
+            prev->pNext = tmp->pNext;
+            else
+            pHead = tmp->pNext;
+            free(tmp);
+            return 0;
+        }
+        prev = tmp;
+        tmp = tmp->pNext;
+    }
+    return -1;//element does not exsist
+}
+int numElements()
+{
+    int num = 0;
+    struct Element* tmp = pHead;
+    while(tmp)
+    {
+        num = num + 1;
+        tmp = tmp->pNext;
+    }
+    return num;
+}
+
+
