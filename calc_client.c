5615b0657c2cde086f4afe57656faa051cfc3722
Jan Ferbr <honza.ferbr@gmail.com>
2021-04-23 11:05:01 +0200

    wtf

--- calc_client.c
+++ calc_client.c
@@ -73,7 +73,7 @@ com_1(char *host)
             printf("key:");
             scanf("%s", &set_value_1_key);
             set_value_1_val1=malloc(255*sizeof(char));
-            res = Get_value(set_value_1_key, set_value_1_val1, &set_value_1_val2, &set_value_1_val3);
+            res = Get_value(set_value_1_key, &set_value_1_val1, &set_value_1_val2, &set_value_1_val3);
             printf("function returned:%i\n",res);
             if (res==0){
                 printf("value1:%s\nvalue2:%d\nvalue3:%f\n",set_value_1_val1,set_value_1_val2,set_value_1_val3);
